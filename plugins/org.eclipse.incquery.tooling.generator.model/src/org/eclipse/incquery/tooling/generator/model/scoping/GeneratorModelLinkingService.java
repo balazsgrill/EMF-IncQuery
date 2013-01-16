92138db3df11b28d50620c9257e9c2565b76c45b
gbalage <balage.g@gmail.com>
2013-01-16 07:07:30 +0100

    eiqgen files accept "plugin:/plugin.id/folder/file.genmodel"-like URIs, and resolve them using PDE. This enables the user to use genmodels from the active TargetPlatform and from the workspace using the same method.

--- plugins/org.eclipse.incquery.tooling.generator.model/src/org/eclipse/incquery/tooling/generator/model/scoping/GeneratorModelLinkingService.java
+++ plugins/org.eclipse.incquery.tooling.generator.model/src/org/eclipse/incquery/tooling/generator/model/scoping/GeneratorModelLinkingService.java
@@ -22,15 +22,21 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.incquery.tooling.generator.model.generatorModel.GeneratorModelPackage;
 import org.eclipse.incquery.tooling.generator.model.generatorModel.GeneratorModelReference;
+import org.eclipse.pde.core.plugin.IPluginModelBase;
+import org.eclipse.pde.core.plugin.PluginRegistry;
 import org.eclipse.xtext.conversion.IValueConverterService;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.linking.impl.DefaultLinkingService;
 import org.eclipse.xtext.nodemodel.ILeafNode;
 import org.eclipse.xtext.nodemodel.INode;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
 
 public class GeneratorModelLinkingService extends DefaultLinkingService {
+
+	private static final String PLUGIN_URI_SCHEME = "plugin";
+
     private static final Logger LOG = Logger.getLogger(GeneratorModelLinkingService.class);
 
     @Inject
@@ -67,7 +73,52 @@ public class GeneratorModelLinkingService extends DefaultLinkingService {
         }
     }
 
+	/**
+	 * Resolve a "plugin:/plugin.id/folder/resource" - like URI to a location
+	 * URL<br>
+	 * This method can find resources from any workspace or target platform
+	 * plugin. This method uses {@link PluginRegistry}.findModel(String
+	 * pluginId) to search for plugins. This method will return the workspace
+	 * version if available, or the target-platform plugin with the highest
+	 * version.
+	 * 
+	 * @param resourceOrNsURI
+	 *            An URI with "plugin:/" scheme
+	 * @return The "file:" or "jar:" URL string of the resource, or null if the
+	 *         given plug-in could not be found.
+	 */
+	private String resolvePluginURI(String resourceOrNsURI) {
+		String path = resourceOrNsURI.substring(PLUGIN_URI_SCHEME.length() + 2);
+		int i = path.indexOf('/');
+		Preconditions.checkArgument(i != -1, "Invalid URI: '" + resourceOrNsURI
+				+ "', a resource must be selected in the plugin!");
+
+		String pluginId = path.substring(0, i);
+		path = path.substring(i);
+
+		IPluginModelBase pluginbase = PluginRegistry.findModel(pluginId);
+		if (pluginbase != null) {
+			String location = pluginbase.getInstallLocation();
+			if (location.endsWith(".jar")) {
+				return "jar:file:" + location + "!" + path;
+			} else {
+				return "file:" + pluginbase.getInstallLocation() + path;
+			}
+		} else {
+			LOG.trace("Invalid plugin ID: '" + pluginId + "'");
+			return null;
+		}
+
+	}
+
     private GenModel loadGenmodel(String resourceOrNsURI, ResourceSet resourceSet) {
+		/* If the given URI is a plugin URI, try to resolve it to a resource */
+		if (resourceOrNsURI.startsWith(PLUGIN_URI_SCHEME + ":/")) {
+			resourceOrNsURI = resolvePluginURI(resourceOrNsURI);
+			if (resourceOrNsURI == null) {
+				return null;
+			}
+		}
         URI uri = null;
         try {
             uri = URI.createURI(resourceOrNsURI);
